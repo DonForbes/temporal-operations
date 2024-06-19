cbabb22e6905712705b8cffda29789c0e82f603d
Donald Forbes <donald.forbes@temporal.io>
2024-06-19 12:02:41 +0100

    First commit

---
+++ src/main/java/com/donald/demo/ops/TemporalOperationsApplication.java
@@ -0,0 +1,13 @@
+package com.donald.demo.ops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemporalOperationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemporalOperationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/donald/demo/ops/TemporalOperationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donald.demo.ops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemporalOperationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
