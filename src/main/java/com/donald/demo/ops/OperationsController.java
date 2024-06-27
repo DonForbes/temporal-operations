ec8bfd8ece9b249c8c5d43c1bf4ebd1a08b04561
Donald Forbes <donald.forbes@temporal.io>
2024-06-27 14:26:23 +0100

    Getting list of namespaces, bad return if a failure.

--- src/main/java/com/donald/demo/ops/OperationsController.java
+++ src/main/java/com/donald/demo/ops/OperationsController.java
@@ -1,6 +1,8 @@
 package com.donald.demo.ops;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,6 +18,7 @@ import org.springframework.vault.client.VaultEndpoint;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.donald.demo.ops.certificates.CertificateUtil;
 import com.donald.demo.ops.certificates.VaultPkiProperties;
@@ -64,10 +67,25 @@ public class OperationsController {
     } // End get current CA
 
     @GetMapping("get-namespaces")
-    public Collection<CloudOperationsNamespace> getNamespaces() {
-        this.namespaceMgmt = new NamespaceMgmt(cloudOpsDetails);
+    public ResponseEntity<Collection<CloudOperationsNamespace>> getNamespaces(@RequestParam(required=true) String apiKey) {
+        cloudOpsDetails.setTmprlApiKey(apiKey);
+        logger.debug("The API key that should be used is [" + apiKey + "]");
+        logger.debug("The API key being used is [" + cloudOpsDetails.getTmprlApiKey() + "]"); 
 
-        return this.namespaceMgmt.getNamespaces();
+        this.namespaceMgmt = new NamespaceMgmt(cloudOpsDetails);
+        Collection<CloudOperationsNamespace> cloudOpsNamespaces = new ArrayList<CloudOperationsNamespace>();
+        try {
+            cloudOpsNamespaces = this.namespaceMgmt.getNamespaces();
+        } catch (io.grpc.StatusRuntimeException e) {
+            logger.error("ERROR - Failed to retrieve namespaces [" + e.getMessage() + "]");
+            CloudOperationsNamespace aCloudOpsNamespace = new CloudOperationsNamespace();
+            aCloudOpsNamespace.setName("-");
+            aCloudOpsNamespace.setActiveRegion("-");
+            aCloudOpsNamespace.setState("Failed to retrieve namespaces [" + e.getMessage() +"]");
+            cloudOpsNamespaces.add(aCloudOpsNamespace);
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.of(Optional.of(cloudOpsNamespaces));
     } // End getNamespaces
 }
 
--- src/main/java/com/donald/demo/ops/namespace/model/CloudOperationDetails.java
+++ src/main/java/com/donald/demo/ops/namespace/model/CloudOperationDetails.java
@@ -15,11 +15,21 @@ public class CloudOperationDetails {
 
     // Set using the environment variable first and if that is not set then use the value from the config file.
     public void setTmprlApiKey(String apiKey) {
+
         String apiKeyEnv = System.getenv("TEMPORAL_CLOUD_API_KEY");
-        if (apiKeyEnv == null) {
-            tmprlApiKey = apiKey;
+        
+        if ((apiKey == null) || (apiKey.length() == 0)) {
+            tmprlApiKey = System.getenv("TEMPORAL_CLOUD_API_KEY");
         }
         else
-            tmprlApiKey = apiKeyEnv;
+        
+            tmprlApiKey = apiKey;
+
+        if ((this.tmprlApiKey == null) || (this.tmprlApiKey.length() == 0))
+        {
+            this.tmprlApiKey = "INVALID";
+            System.err.println("API key not provided to the cloud ops interface by parameter or TEMPORAL_CLOUD_API_KEY environment.  Service will not work");
+        }
+
     } // End setter 
 }
